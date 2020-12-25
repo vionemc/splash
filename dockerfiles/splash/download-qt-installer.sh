755892a91b8449de19a529dba40ac765236b1371
Aminah Nuraini <aminah.nuraini92@gmail.com>
2020-12-26 06:37:42 +0700

    update QT installer URL

--- dockerfiles/splash/download-qt-installer.sh
+++ dockerfiles/splash/download-qt-installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 URL="http://download.qt.io/official_releases/qt/$QT_SHORT_VERSION/$QT_FULL_VERSION/qt-opensource-linux-x64-$QT_FULL_VERSION.run"
-
+URL="http://download.qt.io/official_releases/qt/6.0/6.0.0/single/qt-everywhere-src-6.0.0.tar.xz"
 curl --fail -L -o "$1" ${URL}
