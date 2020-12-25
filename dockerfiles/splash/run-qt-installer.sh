094f56c988077651718c73357c091c228fb3ec11
Aminah Nuraini <aminah.nuraini92@gmail.com>
2020-12-26 06:59:10 +0700

    modified qt file name

--- dockerfiles/splash/run-qt-installer.sh
+++ dockerfiles/splash/run-qt-installer.sh
@@ -3,14 +3,14 @@
 # as well as qt-installer-noninteractive.qs script.
 
 if [ -z ${DISPLAY+x} ]; then
-	command="xvfb-run $1"
+	command="xvfb-run qt-everywhere-src-6.0.0"
 else
-	command="$1"
+	command="qt-everywhere-src-6.0.0"
 fi
 
 chmod +x "$1" && \
 http_proxy="http://localhost:8080" https_proxy="http://localhost:8080" $command --script "$2" \
     | egrep -v '\[[0-9]+\] Warning: (Unsupported screen format)|((QPainter|QWidget))' && \
-ls /opt/qt-$QT_SHORT_VERSION/ && \
+ls /opt/qt-everywhere-src-6.0.0/ && \
 #    cat /opt/qt-$QT_SHORT_VERSION/InstallationLog.txt && \
-cat /opt/qt-$QT_SHORT_VERSION/components.xml
+cat /opt/qt-everywhere-src-6.0.0/components.xml
