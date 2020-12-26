fb5a7a23cf5a2a7340cff39beffb2da0dd09617f
Aminah Nuraini <aminah.nuraini92@gmail.com>
2020-12-26 07:04:18 +0700

    debug qt download folder name

--- dockerfiles/splash/run-qt-installer.sh
+++ dockerfiles/splash/run-qt-installer.sh
@@ -11,6 +11,7 @@ fi
 chmod +x "$1" && \
 http_proxy="http://localhost:8080" https_proxy="http://localhost:8080" $command --script "$2" \
     | egrep -v '\[[0-9]+\] Warning: (Unsupported screen format)|((QPainter|QWidget))' && \
-ls /opt/qt-everywhere-src-6.0.0/ && \
-#    cat /opt/qt-$QT_SHORT_VERSION/InstallationLog.txt && \
-cat /opt/qt-everywhere-src-6.0.0/components.xml
+ls /opt
+# ls /opt/qt-everywhere-src-6.0.0/ && \
+# #    cat /opt/qt-$QT_SHORT_VERSION/InstallationLog.txt && \
+# cat /opt/qt-everywhere-src-6.0.0/components.xml
